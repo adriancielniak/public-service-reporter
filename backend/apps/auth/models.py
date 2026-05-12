35692d3e553fa4085c6380ae94034621c728d764
Panstick <tymoteusz.blaszcz@student.uj.edu.pl>
2026-05-12 22:12:48 +0200

    remove reports from user definition

--- backend/apps/auth/models.py
+++ backend/apps/auth/models.py
@@ -12,9 +12,6 @@ class User(AbstractUser):
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='standard')
     data = models.TextField(blank=True, null=True)
-    # Lista ID raportów jako tablica JSON
-    # inaczej?
-    reports = models.JSONField(default=list, blank=True)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
--- backend/apps/auth/serializers.py
+++ backend/apps/auth/serializers.py
@@ -16,5 +16,5 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'role', 'data', 'reports']
+        fields = ['username', 'password', 'email', 'role', 'data']
 
